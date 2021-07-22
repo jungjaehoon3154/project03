4cc07dc2c0a27adde9c97c08239bc25cddb92315
jungjaehoon3154 <jungjaehoon3154@naver.com>
2021-07-22 16:27:26 +0900

    21-07-22

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
 
-    $('.toggle').click(function(){
-        $(this).toggleClass('active');
-     });
-
     /* slick */
     $('.img_slide').slick({
         dots: true,
@@ -24,8 +20,48 @@ $(document).ready(function(){
     });
     });
 
-    
-    
-   
+    $('.toggle').click(function() {
+        $(this).toggleClass("active");
+    $('.gnb_bg').toggleClass("active");
+    });
 
-});
+    const all = $("#gnb ul li a")
+    const home = $("#gnb .menu1 a")
+    const project = $("#gnb .menu2 a")
+    const about = $("#gnb .menu3 a")
+    const contact = $("#gnb .menu4 a")
+    const bg = $(".gnb_bg")
+
+    all.click(function(){
+        $(bg).removeClass("active");
+        $('.toggle').removeClass("active");
+    });
+    home.mouseover(function(){
+        $(bg).css("background","#233067")
+    });
+    home.mouseout(function(){
+        $(bg).css("background","#000")
+    });
+    project.mouseover(function(){
+        $(bg).css("background","#2f4f4f")
+    });
+    project.mouseout(function(){
+        $(bg).css("background","#000")
+    });
+    about.mouseover(function(){
+        $(bg).css("background","#6f4f28")
+    });
+    about.mouseout(function(){
+        $(bg).css("background","#000")
+    });
+    contact.mouseover(function(){
+        $(bg).css("background","#723838")
+    });
+    contact.mouseout(function(){
+        $(bg).css("background","#000")
+    });
+      
+
+
+});    
+    
